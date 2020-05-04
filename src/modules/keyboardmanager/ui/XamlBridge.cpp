4ba78cff0cb7058e85a9898ee843ed0b9aeeb768
Tomas Raies <tomas.raies@gmail.com>
2020-05-03 23:02:54 -0700

    Fix crashing in Windows Insiders builds
    Added missing unitialization of winrt apartment

--- src/modules/keyboardmanager/ui/XamlBridge.cpp
+++ src/modules/keyboardmanager/ui/XamlBridge.cpp
@@ -227,6 +227,8 @@ void XamlBridge::OnTakeFocusRequested(winrt::Windows::UI::Xaml::Hosting::Desktop
 HWND XamlBridge::InitDesktopWindowsXamlSource(winrt::Windows::UI::Xaml::Hosting::DesktopWindowXamlSource desktopSource)
 {
     HRESULT hr = S_OK;
+    winrt::init_apartment(apartment_type::single_threaded);
+    WindowsXamlManager winxamlmanager = WindowsXamlManager::InitializeForCurrentThread();
 
     auto interop = desktopSource.as<IDesktopWindowXamlSourceNative>();
     // Parent the DesktopWindowXamlSource object to current window
